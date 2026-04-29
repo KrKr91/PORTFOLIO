043c2135fd98c08c7da5b412b3b477ea5641bc71
KrKr91 <shelron.krkr@gmail.com>
2026-04-29 15:21:02 +0200

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.scss'
+import Header from './components/Header';
+import './App.scss';
 
 function App() {
   return (
     <div className="page">
+      <Header />
       <h1>Mon Portfolio</h1>
-      {/* On ajoutera nos composants ici (Header, À propos, Compétences...) */}
+      {/* On ajoutera nos composants ici (À propos, Compétences...) */}
     </div>
   )
 }
 
-export default App
+export default App;
